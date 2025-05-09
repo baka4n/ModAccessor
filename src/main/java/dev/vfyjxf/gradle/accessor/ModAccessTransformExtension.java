f4587c6f6e9b88b8fbd3a3e25c142a0eb7adde4b
vfyjxf <42764413+vfyjxf@users.noreply.github.com>
2025-05-10 02:13:04 +0800

    Revert "Merge pull request #1 from vfyjxf/souce-transform" (#2)
    This reverts commit d537ca8dec902ccc9b7af14a27c5861b2d72977f.

--- src/main/java/dev/vfyjxf/gradle/accessor/ModAccessTransformExtension.java
+++ src/main/java/dev/vfyjxf/gradle/accessor/ModAccessTransformExtension.java
@@ -3,18 +3,19 @@ package dev.vfyjxf.gradle.accessor;
 import org.apache.commons.lang3.StringUtils;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.attributes.Usage;
+import org.gradle.api.artifacts.ExternalModuleDependency;
+import org.gradle.api.artifacts.FileCollectionDependency;
+import org.gradle.api.artifacts.ProjectDependency;
+import org.gradle.api.attributes.Attribute;
 import org.gradle.api.file.ConfigurableFileCollection;
 
 import javax.inject.Inject;
-import java.util.HashMap;
-import java.util.Map;
 
 public abstract class ModAccessTransformExtension {
 
-    private final Project project;
+    static Attribute<Boolean> TRANSFORM_ACCESS = Attribute.of("accessor_plugin_transform_access", Boolean.class);
 
-    private final Map<Configuration, Configuration> transformToSource = new HashMap<>();
+    private final Project project;
 
     public abstract ConfigurableFileCollection getAccessTransformerFiles();
 
@@ -24,18 +25,39 @@ public abstract class ModAccessTransformExtension {
     }
 
     public Configuration createTransformConfiguration(Configuration parent) {
-        var transformer = project.getConfigurations().create("access" + StringUtils.capitalize(parent.getName()), spec -> {
-            spec.setDescription("Configuration for dependencies of " + parent.getName() + " that needs to be transformed");
-            // Don't get transitive deps of already remapped mods
+        var transformAccess = project.getConfigurations().create("access" + StringUtils.capitalize(parent.getName()), spec -> {
+            spec.setDescription("Configuration for dependencies of " + parent.getName() + " that needs to be remapped");
+            spec.setCanBeConsumed(false);
+            spec.setCanBeResolved(false);
             spec.setTransitive(false);
-            final Usage usage = project.getObjects().named(Usage.class, Usage.JAVA_API);
-            spec.attributes(attributes -> attributes.attribute(Usage.USAGE_ATTRIBUTE, usage));
-        });
-        transformToSource.put(transformer, parent);
-        return transformer;
-    }
 
-    public Map<Configuration, Configuration> transformToSourceConfigurations() {
-        return transformToSource;
+            //code based MDG legacy
+            spec.withDependencies(dependencies -> dependencies.forEach(dep -> {
+                if (dep instanceof ExternalModuleDependency externalModuleDependency) {
+                    project.getDependencies().constraints(constraints -> {
+                        constraints.add(parent.getName(), externalModuleDependency.getGroup() + ":" + externalModuleDependency.getName() + ":" + externalModuleDependency.getVersion(), c -> {
+                            c.attributes(a -> a.attribute(TRANSFORM_ACCESS, true));
+                        });
+                    });
+                    externalModuleDependency.setTransitive(false);
+                } else if (dep instanceof FileCollectionDependency fileCollectionDependency) {
+                    project.getDependencies().constraints(constraints -> {
+                        constraints.add(parent.getName(), fileCollectionDependency.getFiles(), c -> {
+                            c.attributes(a -> a.attribute(TRANSFORM_ACCESS, true));
+                        });
+                    });
+                } else if (dep instanceof ProjectDependency projectDependency) {
+                    project.getDependencies().constraints(constraints -> {
+                        constraints.add(parent.getName(), projectDependency.getDependencyProject(), c -> {
+                            c.attributes(a -> a.attribute(TRANSFORM_ACCESS, true));
+                        });
+                    });
+                    projectDependency.setTransitive(false);
+                }
+            }));
+        });
+        parent.extendsFrom(transformAccess);
+        transformAccess.getAttributes().attribute(TRANSFORM_ACCESS, false);
+        return transformAccess;
     }
 }
